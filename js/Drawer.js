f93cb302487f3316e3db7af450836010793ba71f
deng <786339231@qq.com>
2020-08-11 18:44:45 +0800

    禁止移动端拖动

--- js/Drawer.js
+++ js/Drawer.js
@@ -87,6 +87,10 @@ class Drawer {
         this.ui.saveBtn.onclick=()=>{
             this.save()
         }
+
+        document.body.addEventListener('touchmove', function (e) {
+            e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
+        }, {passive: false}); //passive 参数不能省略，用来兼容ios和android
     }
 
     initTouchDrawEvent() {
