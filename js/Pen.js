be138c7bdef2096b78f82e472f05fdabc1c3e821
deng <786339231@qq.com>
2020-08-11 19:40:24 +0800

    修复橡皮擦导致logo变空

--- js/Pen.js
+++ js/Pen.js
@@ -7,13 +7,13 @@ class Pen {
         this.parentNode = parentNode
     }
 
-    render(name = 'pen',drawer) {
-        const {ctx,pensDom,ui} = drawer
+    render(name = 'pen', drawer) {
+        const {ctx, pensDom, ui} = drawer
         this.pensDom = pensDom
         this.ctx = ctx
         this.ui = ui
-        if(this.color==='#ffffff'){
-            name="clean"
+        if (this.color === '#ffffff') {
+            name = "clean"
         }
         let html = `<svg class="icon" aria-hidden="true">
                             <use xlink:href="#icon-${name}"></use>
@@ -22,27 +22,29 @@ class Pen {
         temp.innerHTML = html
 
         let dom = temp.content.firstChild
-        dom.style.color = this.color==='#ffffff'?'black':this.color
+        dom.style.color = this.color === '#ffffff' ? 'black' : this.color
         dom.classList.add('pen')
         this.parentNode.appendChild(dom)
         this.dom = dom
-        this.dom.onclick=this.click
+        this.dom.onclick = this.click
     }
-    click=()=>{
+
+    click = () => {
         this.active = !this.active
-        this.pensDom.forEach((d)=>{
+        this.pensDom.forEach((d) => {
             console.log(d);
             d.dom.classList.remove('active')
 
         })
-        if(this.active){
+        if (this.active) {
             this.dom.classList.add('active')
             this.ctx.strokeStyle = this.color
-            this.ui.paintColor.value=this.color
-            this.ui.logo.style.color=this.color
+            this.ui.paintColor.value = this.color
+            this.ui.logo.style.color = this.color === '#ffffff' ? 'black' : this.color
         }
     }
 
 
 }
+
 export default Pen
